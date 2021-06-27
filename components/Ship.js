e72f1bdcf9e96d71547689d1147f13b0c244718d
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-06-27 19:28:11 -0400

    ship passes tests

--- components/Ship.js
+++ components/Ship.js
@@ -2,6 +2,16 @@ class Ship {
   constructor(name, length) {
     this.name = name;
     this.points = new Array(length).fill(0);
+    this.hit = (point) => {
+      this.points[point] = 1;
+    };
+    this.isSunk = () => {
+      if (this.points.every((point) => point === 1)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
 
--- components/Ship.test.js
+++ components/Ship.test.js
@@ -11,3 +11,15 @@ test("battleship.hit marks the correct point as hit", () => {
   battleship.hit(2);
   expect(battleship.points).toStrictEqual([0, 0, 1, 0]);
 });
+
+test("battleship.isSunk returns true only if all points hit", () => {
+  const battleship = new Ship("battleship", 4);
+  battleship.hit(0);
+  expect(battleship.isSunk()).toBe(false);
+  battleship.hit(1);
+  expect(battleship.isSunk()).toBe(false);
+  battleship.hit(2);
+  expect(battleship.isSunk()).toBe(false);
+  battleship.hit(3);
+  expect(battleship.isSunk()).toBe(true);
+});
